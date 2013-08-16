ae16b7475c01557d847821165c7483fa90779a2e
Benjamin Daschel <coffeemaxed@gmail.com>
2013-08-16 06:26:27 -0700

    * made the empty list view less ugly

--- gen/com/example/bettershoppinglist/R.java
+++ gen/com/example/bettershoppinglist/R.java
@@ -22,16 +22,17 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int shopping_cart=0x7f020001;
     }
     public static final class id {
         public static final int action_settings=0x7f080007;
-        public static final int button1=0x7f080003;
+        public static final int addItem_button=0x7f080002;
         public static final int checkBox1=0x7f080005;
         public static final int editText1=0x7f080006;
         public static final int empty_view=0x7f080001;
         public static final int fragmentContainer=0x7f080004;
+        public static final int shoppingCart_image=0x7f080003;
         public static final int shoppingListFragment=0x7f080000;
-        public static final int textView1=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -45,6 +46,7 @@ public final class R {
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
+        public static final int add_item=0x7f050005;
         public static final int app_name=0x7f050000;
         public static final int empty_text=0x7f050004;
         public static final int hello_world=0x7f050002;
