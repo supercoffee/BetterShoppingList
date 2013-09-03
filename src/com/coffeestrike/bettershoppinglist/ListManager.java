81b126ef2b03f15fe575159a6232b50bb34c9022
Benjamin Daschel <coffeemaxed@gmail.com>
2013-09-03 12:56:15 -0700

    *added more functionality to ListManager

--- src/com/coffeestrike/bettershoppinglist/ListManager.java
+++ src/com/coffeestrike/bettershoppinglist/ListManager.java
@@ -1,14 +1,27 @@
 package com.coffeestrike.bettershoppinglist;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
+import org.json.JSONArray;
+
 import android.content.Context;
+import android.util.Log;
 
 /*
  * This class is a singleton.  Only one will ever be allowed
  * to exist at any given time.
  */
 public class ListManager {
+	
+	private static final String TAG = "ListManager";
+
+	//This file should contain the names of all the lists being managed.
+	private static String FILENAME = "listmanager.json";
+	
+	private static String FILE_JSON_KEY = "listfilename";
 	
 	private static ListManager sListManager;
 	private ArrayList<ShoppingList> mAllLists;
@@ -34,6 +47,33 @@ public class ListManager {
 	 */
 	public void prepare(){
 		//Load the lists now
+	}
+	
+	/**
+	 * @return True if all lists were able to save successfully.
+	 */
+	public boolean save(){
+		JSONArray jArray = new JSONArray();
+		for(ShoppingList list : mAllLists){
+			jArray.put(list.getListId().toString());
+		}
+		
+		/*
+		 * Now that the JSON object has been created, we need to write it to storage.
+		 */
+		
+		try {
+			ObjectOutputStream oos = new ObjectOutputStream(mAppContext.
+					openFileOutput(FILENAME, Context.MODE_PRIVATE));
+		} catch (FileNotFoundException e) {
+			// Shouldn't happen because the file will be created, but WTH.
+			e.printStackTrace();
+		} catch (IOException e) {
+			Log.e(TAG, "Couldn't write list names to file", e);
+		}
+		
+		
+		return false;
 	}
 	
 	/**
