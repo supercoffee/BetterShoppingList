0af81ca83a5e8744283b591260e684a36bfb477e
Benjamin Daschel <coffeemaxed@gmail.com>
2014-01-03 16:35:02 -0800

    * forgot to include OnDownloadFinishedListener class in last commit

---
+++ src/com/coffeestrike/bettershoppinglist/extra/OnDownloadFinishedListener.java
@@ -0,0 +1,9 @@
+package com.coffeestrike.bettershoppinglist.extra;
+
+import android.os.Bundle;
+
+public interface OnDownloadFinishedListener {
+	
+	public void onDownloadFinished(Bundle bundle);
+
+}
